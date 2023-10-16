c741043788380ea7d5484a323a913e28de0d6b2f
matheoszxx <matheuszxx1@outlook.com>
2023-10-16 19:58:11 -0300

    Diagramação de classes do iphone

---
+++ src/Interfaces/AparelhoTelefonico.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+public interface AparelhoTelefonico {
+    public void ligar(String number);
+    public void atender(String number);
+    public void iniciarCorreioVoz(String message, String remittee);
+}
---
+++ src/application/Iphone.java
@@ -0,0 +1,65 @@
+package application;
+
+import Interfaces.AparelhoTelefonico;
+import Interfaces.NavegadorInternet;
+import Interfaces.ReprodutorMusical;
+
+public class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
+
+	@Override
+	public void tocar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pausar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selecionarMusica() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exibirPagina(String url) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void adicionarNovaAba(String url) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizarPagina() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ligar(String number) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atender(String number) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iniciarCorreioVoz(String message, String remittee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+		
+
+}
